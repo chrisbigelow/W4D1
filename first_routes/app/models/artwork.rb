69506b552c4422243ff99325844b8645c6b3af1e
Chris Bigelow <chrisbigelow25@gmail.com>
2017-10-23 16:51:06 -0700

    added user, artwork, and artwork share tables and models

---
+++ first_routes/app/models/artwork.rb
@@ -0,0 +1,19 @@
+class Artwork < ApplicationRecord
+  validates :title, :image_url, :artist_id, presence: true
+  validates :image_url, uniqueness: true
+  validates :artist_id, uniqueness: { scope: :title }
+
+
+  has_many :artwork_shares,
+  foreign_key: :artwork_id,
+  primary_key: :id,
+  class_name: :ArtworkShare
+
+  has_many :shared_viewers,
+  through: :artwork_shares,
+  source: :viewer
+
+  
+
+
+end
--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
-  # validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+
+  has_many :artwork_shares,
+  foreign_key: :user_id,
+  primary_key: :id,
+  class_name: :ArtworkShare
+
+  has_many :shared_artworks,
+  through: :artwork_shares,
+  source: :artwork
+
+
+
+
 end
